85632a9710e917ad5e84d7670781158ca4e04ac0
Emi Taylor <letailleur.emilie@gmail.com>
2017-03-16 16:27:31 +0100

    front

--- core/countdown.js
+++ core/countdown.js
@@ -18,8 +18,9 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="count"
-    document.getElementById("count").innerHTML = /*days + "d " + */hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("hours").innerHTML = hours + "h";
+    document.getElementById("minutes").innerHTML = minutes + "m";
+    document.getElementById("seconds").innerHTML = seconds + "s";
 
     // If the count down is over, write some text
     if (distance < 0) {
