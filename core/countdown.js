c8c0a8abe3331d844a3e558397030eddfd620694
Emi Taylor <letailleur.emilie@gmail.com>
2017-03-29 01:28:43 +0200

    update background image

--- core/countdown.js
+++ core/countdown.js
@@ -18,9 +18,9 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="count"
-    document.getElementById("hours").innerHTML = hours + "h";
-    document.getElementById("minutes").innerHTML = minutes + "m";
-    document.getElementById("seconds").innerHTML = seconds + "s";
+    document.getElementById("hours").innerHTML = hours + " h";
+    document.getElementById("minutes").innerHTML = minutes + " m";
+    document.getElementById("seconds").innerHTML = seconds + " s";
 
     // If the count down is over, write some text
     if (distance < 0) {
